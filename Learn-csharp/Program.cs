882ddb6eca501a489fe3b3e39523caac4dc9926a
ADMIN-PC\Admin <dinhsyduyet@gmail.com>
2022-05-25 21:02:28 +0700

    Add project files.

---
+++ Learn-csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+
+namespace LearnC
+{
+    class Program
+    {
+        static public int a;
+        static void print(int n)
+        {   
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("I love you 3000" + i+"\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a" +  a);
+
+            print(a);
+           
+        }
+    }
+}
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+
